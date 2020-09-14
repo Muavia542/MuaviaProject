ddccea6affa592aa8dfb6eb3fadaaba938b9258a
Muavia542 <muaviakhan342@gmail.com>
2020-09-14 21:39:32 +0500

    first commit

---
+++ AmeerMuaviaPro/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AmeerMuaviaPro.Startup))]
+namespace AmeerMuaviaPro
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
